2f6d7e2da2bc77aa507c642045aa309699092f38
ram-synechron <Ramachandran.Veluthattil@ingramcontent.com>
2017-03-04 23:41:21 -0600

    initial commit

---
+++ Plugin2/Controllers/Plugin2Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Plugin2.Models;
+using System.ComponentModel.Composition;
+
+namespace Plugin2.Controllers
+{
+    [Export("Plugin2",typeof(IController))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class Plugin2Controller : Controller
+    {
+        // GET: Plugin2
+        public ActionResult Index()
+        {
+            ShopOrderModel mdl = new ShopOrderModel
+            {
+                CustomerId = 1,
+                CustomerName = "Test",
+                ShopOrderId = 1,
+                sessionInfo = Session["test"].ToString()
+            };
+            ViewBag.shoporder = mdl;
+            return View();
+        }
+    }
+}
---
+++ Plugin2/Models/ShopOrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plugin2.Models
+{
+    public class ShopOrderModel
+    {
+        public int ShopOrderId { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+
+        public string sessionInfo { get; set; }
+    }
+}
